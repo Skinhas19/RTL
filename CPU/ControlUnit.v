3414f9b3bdaa284f07347a7a72e99d447b768f0d
Skinhas <lucassantosmonteiro010@gmail.com>
2026-06-01 13:54:17 -0300

    5

--- CPU/ControlUnit.v
+++ CPU/ControlUnit.v
@@ -2,7 +2,7 @@ module control (
     input wire clk,
     input wire rst,
     input wire [6:0] cmd_in,
-    input p_error,
+    input wire p_error,
     output reg aluin_reg_en,
     output reg datain_reg_en,
     output reg memoryWrite,memoryRead,selmux2,
@@ -18,6 +18,11 @@ module control (
     localparam STORE     = 2'd3; // Salvando o resultado
 
     reg [1:0] current_state,next_state;
+
+    wire [1:0]cmd_muxA=cmd_in[6:5];
+    wire [1:0]cmd_muxB=cmd_in[4:3];
+    wire [2:0]cmd_opcode=cmd_in[2:0];
+    
 
     always @(posedge clk) begin
         if (rst) begin
@@ -39,26 +44,59 @@ always @(*) begin
     invalid_data=0;
     in_select_a=2'b00;
     in_select_b=2'b00;
-    
+
+    // Repassa o opcode de 3 bits estendido para 4 bits para a ULA
+    opcode = {1'b0, cmd_opcode};
+    invalid_data=(p_error&&(cmd_muxA==2'b11||cmd_muxB==2'b11));
 case (current_state)
             
             RESET: begin
                 
                 datain_reg_en = 1; // Permite ler um novo comando
-                
                 // Se chegou um comando válido, vai para a próxima etapa
-                if (opcode != 4'b0000) begin 
-                    next_state = FETCH;
-                end
                 next_state=FETCH;
             end
-            FETCH:begin
-                aluin_reg_en=1;
 
+            FETCH:begin
+
+                in_select_a = cmd_muxA; 
+                in_select_b = cmd_muxB;
+
+                if (cmd_opcode == 3'b100 || cmd_opcode == 3'b111) begin
+                    aluin_reg_en = 0;
+                end else begin
+                    aluin_reg_en = 1;
+                end
+                next_state=EXECUTE;
             end
 
-endcase
+            EXECUTE: begin
+                case(cmd_opcode)
+                3'b000,3'b001,3'b010,3'b011 begin //ADD,SUB,MUL,DIV
+                    aluout_reg_en=1;
+                    selmux2=0;
+                end
+                3'b101 begin    //LOAD
+                    memoryRead=1;
+                    selmux2=1;
+                    aluout_reg_en=1;
+                end
+                default: ;//STORE,NOP
+            endcase
+            next_state=STORE;
+            end
 
+            STORE: begin
+                cpu_rdy=1;
+                datain_reg_en=1;
+                if(cmd_opcode==110)begin
+                    memoryWrite=1;
+                end
+            next_state=FETCH;
+
+            default next_state=RESET;
+            end
+endcase
 end
 
 
